befd7ce84cb7b000a51396c106b9078430770d95
michael-rayner92 <39574445+michael-rayner92@users.noreply.github.com>
2022-05-31 23:44:15 +0200

    feat(component): :sparkles: article tracker component added
    - show article history

---
+++ src/components/article-tracker/Step.tsx
@@ -0,0 +1,41 @@
+import type { FC } from 'react';
+import { Box, BoxProps, Stack, Text } from '@chakra-ui/react';
+
+const getStatusColor = (currentCount: number, isActive: boolean, isCompleted: boolean) => {
+  if (currentCount >= 6) return 'error';
+  if (isActive || isCompleted) return 'accent';
+  return 'inherit';
+};
+
+interface StepProps extends BoxProps {
+  title: string;
+  description: string;
+  isCompleted: boolean;
+  isActive: boolean;
+  currentCount: number;
+}
+
+export const Step: FC<StepProps> = props => {
+  const { title, description, isActive, isCompleted, currentCount } = props;
+  const borderColor = getStatusColor(currentCount, isActive, isCompleted);
+
+  return (
+    <Box
+      flex="1"
+      py={{ base: '2', md: '3' }}
+      ps={{ base: '3', md: '0' }}
+      borderTopWidth={{ base: '0', md: '4px' }}
+      borderLeftWidth={{ base: '4px', md: '0' }}
+      borderColor={borderColor}
+    >
+      <Stack spacing="0.5">
+        <Text color="emphasized" fontWeight="medium">
+          {title}
+        </Text>
+        <Text color="muted">{description}</Text>
+      </Stack>
+    </Box>
+  );
+};
+
+Step.displayName = 'Step';
